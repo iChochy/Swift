a7b2464b119c52ac93a4de586e99f40dad2ebe92
MLeo <lb52406@qq.com>
2018-08-28 12:02:37 +0800

    init

---
+++ SwiftCode/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  swift-11
+//
+//  Created by MLeo on 2018/8/27.
+//  Copyright © 2018年 swift. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+
+    override func loadView() {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+   
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+}
+
